b588d5e419010fc44daba893d7dfbba9039e27de
mannev1 <vmanne3@illinois>
2020-05-05 16:06:06 -0400

    removed use of magic numbers

--- include/mylibrary/Image.h
+++ include/mylibrary/Image.h
@@ -7,13 +7,15 @@
 
 #include <cinder/Surface.h>
 #include <cinder/gl/Context.h>
-
 #include <string>
 
 namespace mylibrary {
-
+  // checks validity of image provided by the user
   std::string CheckValidFile(std::string file_path);
+  // returns vector of textures cropped to the size of a tile in a 3x3 game
   std::vector<cinder::gl::Texture2dRef> MakeTextureVec(std::string filepath);
+  const int tile_width_ = 267;
+  const int tile_height_ = 200;
 
 }  // namespace mylibrary
 
--- src/Image.cc
+++ src/Image.cc
@@ -4,50 +4,53 @@
 
 #include "mylibrary/Image.h"
 #include <mylibrary/GameBoard.h>
-
 #include "cinder/gl/Texture.h"
 #include <string>
 
 namespace mylibrary {
+cinder::gl::Texture2dRef MakeTexture(cinder::Surface& surface, int x, int y) {
+  // hard-coded to the size of a tile in a 3x3 grid
+  cinder::Surface new_surface(tile_width_,tile_height_,false);
 
-cinder::gl::Texture2dRef MakeText(cinder::Surface& surface, int x, int y) {
-  cinder::Surface s(267,200,false);
-  for (int i = 0; i < 267; i++) {
-    for (int j = 0; j < 200; j++) {
-      s.setPixel(cinder::ivec2(i,j),surface.getPixel(cinder::ivec2(i+x,j+y)));
+  // set every pixel in new_surface to the color of the corresponding pixel in surface
+  for (int i = 0; i < tile_width_; i++) {
+    for (int j = 0; j < tile_height_; j++) {
+      auto color = surface.getPixel(ci::ivec2(i+x,j+y));
+      new_surface.setPixel(ci::ivec2(i,j), color);
     }
   }
-  cinder::gl::TextureRef texture = cinder::gl::Texture2d::create(s);
-  return texture;
+  return ci::gl::Texture2d::create(new_surface);
 }
 
 std::string CheckValidFile(std::string file_path) {
   if (std::ifstream(file_path).fail()) {
-    return "please input a valid filepath";
-  } else if (cinder::loadImage(file_path)->getWidth() < 267) {
-    return "please pick an image that has a width greater than 267 pixels";
-  } else if (cinder::loadImage(file_path)->getWidth() < 200) {
-    return "please pick an image that has a height greater than 200 pixels";
+    return "please input a valid filepath\n";
+  // if width too small
+  } else if (ci::loadImage(file_path)->getWidth() < tile_width_) {
+    return "please pick an image that has a width greater than 267 pixels\n";
+  // if height too small
+  } else if (ci::loadImage(file_path)->getWidth() < tile_height_) {
+    return "please pick an image that has a height greater than 200 pixels\n";
   } else {
+    // valid file
     return " ";
   }
 }
 
 std::vector<cinder::gl::Texture2dRef> MakeTextureVec(std::string filepath) {
+  ci::Surface surface;
   if (CheckValidFile(filepath) != " ") {
     std::cerr << CheckValidFile(filepath);
   }
+  surface = ci::Surface(cinder::loadImage(filepath));
+  std::vector<ci::gl::Texture2dRef> toReturn;
 
-  cinder::Surface surface(cinder::loadImage(filepath));
-  std::vector<cinder::gl::Texture2dRef> toReturn;
-
-  for (int y = 0; y < 600; y = y+200) {
-    for (int x = 0; x < 801; x = x+267) {
-      toReturn.push_back(MakeText(surface, x,y));
+  // makes textures for the 9 tiles in a 3x3 game
+  for (int y = 0; y < (tile_height_*3); y = y + tile_height_) {
+    for (int x = 0; x < (tile_width_*3); x = x + tile_width_) {
+      toReturn.push_back(MakeTexture(surface, x, y));
     }
   }
   return toReturn;
 }
-
-
 }  // namespace mylibrary
